654a5fc6c16f7f055443474fa349b81730643e91
men9xuan <nuevodiia@gmail.com>
2024-09-02 14:42:10 -0400

    add support for m3 2.2 lite bwr

--- firmware/oepl_efr32_hwtypes.c
+++ firmware/oepl_efr32_hwtypes.c
@@ -353,6 +353,8 @@ uint8_t oepl_efr32xg22_get_oepl_hwid(void)
         return SOLUM_M3_BWR_16;
       case STYPE_SIZE_022:
         return SOLUM_M3_BWR_22;
+      case STYPE_SIZE_022_LITE:
+        return SOLUM_M3_BWR_22_LITE;
       case STYPE_SIZE_026:
         return SOLUM_M3_BWR_26;
       case STYPE_SIZE_029:
@@ -505,6 +507,10 @@ bool oepl_efr32xg22_get_displayparams(oepl_efr32xg22_displayparams_t* displaypar
         displayparams->mirrorY = true;
         break;
       case STYPE_SIZE_022:
+        displayparams->swapXY = true;
+        displayparams->xoffset = 8;
+        break;
+      case STYPE_SIZE_022_LITE:
         displayparams->swapXY = true;
         displayparams->xoffset = 8;
         break;
